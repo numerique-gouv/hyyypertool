2a184d878bdea2b502e36ec4371ca0d76145e399
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-15 12:37:59 +0100

    debug(scalingo): dummy server

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+//
+
+import Elysia from "elysia";
+import pkg from "../package.json";
+
+//
+
+const app = new Elysia()
+  .get("/healthz", () => `healthz check passed`)
+  .get("/livez", () => `livez check passed`)
+  .get("/readyz", () => `readyz check passed`)
+  .listen(Bun.env.PORT!, (srv) => {
+    console.log(
+      `${pkg.name}: http://${srv.hostname}:${srv.port}\n` +
+        `- Environment: ${Bun.env.NODE_ENV}\n` +
+        `- Version: v${pkg.version}`
+    );
+  });
