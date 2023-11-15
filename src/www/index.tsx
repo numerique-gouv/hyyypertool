b7c91ef140df642bd7993632cd1bdb5185b85250
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-15 17:59:23 +0100

    chore: remove generated style

--- src/www/index.tsx
+++ src/www/index.tsx
@@ -2,4 +2,17 @@
 
 import type { ElysiaWWW } from ":www";
 
-export default (www: ElysiaWWW) => www.get("/", () => <h1>Hello Raaoph</h1>);
+export default (www: ElysiaWWW) =>
+  www.get("/", () => (
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>
+          H{Array.from({ length: Math.random() * 5 }).fill("y")}pertool
+        </title>
+        <link rel="stylesheet" href="/public/tailwind/styles.css"></link>
+      </head>
+      <h1>Hello Raaoph</h1>
+    </html>
+  ));
