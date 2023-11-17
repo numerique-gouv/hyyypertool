ad6522d21cea2c565a1db2355f603c64a6955971
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-17 19:26:51 +0100

    debug(scalingo): test custom local buildpack (12)

--- .scalingo/buildpack/profile/node.sh
+++ .scalingo/buildpack/profile/node.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export PATH="$HOME/.scalingo/node/bin:$HOME/.scalingo/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
-export NODE_HOME="$HOME/.scalingo/node"
-export NODE_ENV=${NODE_ENV:-production}
-export NODE_EXTRA_CA_CERTS=${NODE_EXTRA_CA_CERTS:-/usr/share/ca-certificates/Scalingo/scalingo-database.pem}
+export FNM_DIR="$HOME/.scalingo/fnm"
+# export PATH="$HOME/.scalingo/node/bin:$HOME/.scalingo/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
+# export NODE_HOME="$HOME/.scalingo/node"
+# export NODE_ENV=${NODE_ENV:-production}
+# export NODE_EXTRA_CA_CERTS=${NODE_EXTRA_CA_CERTS:-/usr/share/ca-certificates/Scalingo/scalingo-database.pem}
