8c24793f159f1a76f685b543525b2482a357950d
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-17 20:49:39 +0100

    debug(scalingo): test custom local buildpack (21)

--- .scalingo/buildpack/profile/n.sh
+++ .scalingo/buildpack/profile/n.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export N_PREFIX="$HOME/.scalingo/n"
-export PATH="$N_INSTALL:$PATH:$HOME/bin:$HOME/node_modules/.bin"
+export PATH="$N_PREFIX:$PATH:$HOME/bin:$HOME/node_modules/.bin"
