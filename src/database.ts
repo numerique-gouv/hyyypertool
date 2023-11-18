d18961738119cca7626884acec5ea6ed0a6efe10
Douglas DUTEIL <douglasduteil@gmail.com>
2023-11-18 04:41:02 +0100

    debug(scalingo): test custom local buildpack (66)

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import env from ":env";
 import { PrismaClient } from "@prisma/client";
 
 //
-console.log(env.DATABASE_URL);
+
 export const prisma = new PrismaClient({
   datasourceUrl: env.DATABASE_URL,
   log: ["query", "info", "warn", "error"],
