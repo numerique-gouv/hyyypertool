b9a3a5208dcafc57b2fe8dedade424de285b0b60
Douglas DUTEIL <douglasduteil@gmail.com>
2024-01-16 18:41:52 +0100

    feat: share the nonce (4)

--- src/legacy/routes/index.tsx
+++ src/legacy/routes/index.tsx
@@ -2,7 +2,7 @@
 
 import type { Csp_Context } from ":common/csp_headers";
 import { Id_Schema } from ":common/schema";
-import { hyyyyyypertool_session, type Session_Context } from ":common/session";
+import { type Session_Context } from ":common/session";
 import { LegacyPage } from ":legacy/page";
 import { Main_Layout, userinfo_to_username } from ":ui/layout/main";
 import { zValidator } from "@hono/zod-validator";
@@ -13,7 +13,6 @@ import { jsxRenderer } from "hono/jsx-renderer";
 
 export default new Hono<Session_Context & Csp_Context>()
   .use("*", jsxRenderer(Main_Layout, { docType: true, stream: true }))
-  .use("*", hyyyyyypertool_session)
   .get(
     "/",
     zValidator("query", Id_Schema.partial().default({})),
