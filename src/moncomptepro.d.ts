d22e8e97721f2f60523198ccf25d9c0fd312136a
Douglas DUTEIL <douglasduteil@gmail.com>
2024-01-17 19:20:33 +0100

    feat(moderations): support legacy types

--- src/moncomptepro.d.ts
+++ src/moncomptepro.d.ts
@@ -4,6 +4,12 @@
 /// <reference types="moncomptepro/src/types/user.d.ts" />
 /// <reference types="moncomptepro/src/types/user-organization-link.d.ts" />
 
-export type MCP_Moderation = Moderation;
+export type MCP_Moderation = Moderation | LegacyModeration;
 export type MCP_User = User;
 export type MCP_UserOrganizationLink = UserOrganizationLink;
+
+//
+
+interface LegacyModeration {
+  type: "ask_for_sponsorship" | "big_organization_join";
+}
