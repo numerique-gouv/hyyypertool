74665758b2bae102e7b352d21383bde23f2168b0
Douglas DUTEIL <douglasduteil@gmail.com>
2024-01-29 12:53:47 +0100

    feat(moderations): make the whole moderation line clickable
    Closes https://github.com/betagouv/hyyypertool/issues/11

--- src/ui/table/index.ts
+++ src/ui/table/index.ts
@@ -7,6 +7,8 @@ import { tv } from "tailwind-variants";
 export const row = tv({
   variants: {
     is_active: { true: "bg-green-300" },
-    is_clickable: { true: "cursor-pointer" },
+    is_clickable: {
+      true: "cursor-pointer hover:!bg-[--background-alt-grey-hover]",
+    },
   },
 });
