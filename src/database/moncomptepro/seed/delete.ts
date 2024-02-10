fa00e90cfeb034210907e4122f0bd237c6c8295c
Douglas DUTEIL <douglasduteil@gmail.com>
2024-02-10 10:12:23 +0100

    ci(github): mute logs on ci (2)

--- src/database/moncomptepro/seed/delete.ts
+++ src/database/moncomptepro/seed/delete.ts
@@ -5,30 +5,35 @@ import type { MonComptePro_PgDatabase } from "../moncomptepro_pg";
 
 //
 
+const logger =
+  env.DEPLOY_ENV === "preview"
+    ? console
+    : { log(_message?: any, ..._optionalParams: any[]) {} };
+
+//
+
 export async function delete_database(db: MonComptePro_PgDatabase) {
   try {
     const users_organizations = await db.delete(schema.users_organizations);
-    console.log(
+    logger.log(
       `🚮 ${users_organizations.command} ${users_organizations.rowCount} users_organizations`,
     );
     const users_oidc_clients = await db.delete(schema.users_oidc_clients);
-    console.log(
+    logger.log(
       `🚮 ${users_oidc_clients.command} ${users_oidc_clients.rowCount} users_oidc_clients`,
     );
     const oidc_clients = await db.delete(schema.oidc_clients);
-    console.log(
+    logger.log(
       `🚮 ${oidc_clients.command} ${oidc_clients.rowCount} oidc_clients`,
     );
     const users = await db.delete(schema.users);
-    console.log(`🚮 ${users.command} ${users.rowCount} users`);
+    logger.log(`🚮 ${users.command} ${users.rowCount} users`);
     const organizations = await db.delete(schema.organizations);
-    console.log(
+    logger.log(
       `🚮 ${organizations.command} ${organizations.rowCount} organizations`,
     );
     const moderations = await db.delete(schema.moderations);
-    console.log(
-      `🚮 ${moderations.command} ${moderations.rowCount} moderations`,
-    );
+    logger.log(`🚮 ${moderations.command} ${moderations.rowCount} moderations`);
   } catch (err) {
     console.error("Something went wrong...");
     console.error(err);
