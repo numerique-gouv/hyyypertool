189b6385e1ab2925d5f467d34fe3ea7cdb754a42
Douglas DUTEIL <douglasduteil@gmail.com>
2024-02-26 17:15:03 +0100

    feat(moderation): wait a second

--- packages/~/infra/moncomptepro/database/src/seed/delete.ts
+++ packages/~/infra/moncomptepro/database/src/seed/delete.ts
@@ -9,25 +9,25 @@ import type { MonComptePro_PgDatabase } from "../index";
 export async function delete_database(db: MonComptePro_PgDatabase) {
   try {
     const users_organizations = await db.delete(schema.users_organizations);
-    consola.info(
+    consola.verbose(
       `🚮 ${users_organizations.command} ${users_organizations.rowCount} users_organizations`,
     );
     const users_oidc_clients = await db.delete(schema.users_oidc_clients);
-    consola.info(
+    consola.verbose(
       `🚮 ${users_oidc_clients.command} ${users_oidc_clients.rowCount} users_oidc_clients`,
     );
     const oidc_clients = await db.delete(schema.oidc_clients);
-    consola.info(
+    consola.verbose(
       `🚮 ${oidc_clients.command} ${oidc_clients.rowCount} oidc_clients`,
     );
     const users = await db.delete(schema.users);
-    consola.info(`🚮 ${users.command} ${users.rowCount} users`);
+    consola.verbose(`🚮 ${users.command} ${users.rowCount} users`);
     const organizations = await db.delete(schema.organizations);
-    consola.info(
+    consola.verbose(
       `🚮 ${organizations.command} ${organizations.rowCount} organizations`,
     );
     const moderations = await db.delete(schema.moderations);
-    consola.info(
+    consola.verbose(
       `🚮 ${moderations.command} ${moderations.rowCount} moderations`,
     );
   } catch (err) {
