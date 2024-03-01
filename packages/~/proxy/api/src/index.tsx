44473cd58b43997031ebcac6024c371e391f187e
Douglas Duteil <douglasduteil@gmail.com>
2024-03-01 17:21:42 +0100

    refacto(proxy): move api internal code (#122)

--- packages/~/proxy/api/src/index.tsx
+++ packages/~/proxy/api/src/index.tsx
@@ -4,9 +4,9 @@ import { Hono } from "hono";
 
 //
 
-export const proxy = new Hono().get(
-  "/proxy/localhost:3000/*",
-  ({ req, redirect }) => {
+export default new Hono().get(
+  "/localhost:3000/*",
+  function GET({ req, redirect }) {
     const uri = new URL(
       req.url.replace("/proxy/localhost:3000", ""),
       "http://localhost:3000/",
