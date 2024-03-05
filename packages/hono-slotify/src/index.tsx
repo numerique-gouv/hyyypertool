a5a461bf6bdad55363c16c05a214358a46f08a84
Douglas Duteil <douglasduteil@gmail.com>
2024-03-05 14:34:47 +0100

    feat(hono-slotify): initialisation (#127)

---
+++ packages/hono-slotify/src/index.tsx
@@ -0,0 +1,66 @@
+//
+
+import { cloneElement, isValidElement, type Child, type FC } from "hono/jsx";
+import { raw } from "hono/utils/html";
+
+//
+// Inspired by https://github.com/andrey-skl/react-slotify/blob/6317a858696544c4d17db3d1823625beece3412c/src/index.tsx
+//
+
+interface RendererProps {
+  childs: Child | undefined;
+  children?: Child | undefined;
+}
+
+type RendererType<P> = FC<RendererProps & P>;
+
+type SlotProps<P> = {
+  showChildren?: boolean;
+  restProps?: P & { defaultChildren: Child };
+};
+
+type NormalOrFunctionChildren<P> =
+  | Child
+  | undefined
+  | ((props: P & { defaultChildren: Child | undefined }) => Child | undefined);
+
+type SlotType<P> = FC<
+  SlotProps<P> & { children?: NormalOrFunctionChildren<P> }
+> & {
+  Renderer: RendererType<P>;
+};
+export function createSlot<P extends {}>() {
+  const Slot: SlotType<P> = (({ children, showChildren, restProps }) => {
+    if (!showChildren) {
+      return <></>;
+    }
+    if (typeof children === "function" && restProps) {
+      return children(restProps);
+    }
+    return <>{children}</>;
+  }) as SlotType<P>;
+
+  const Renderer: RendererType<P> = ({ childs, children, ...restProps }) => {
+    if (childs === undefined) {
+      return raw(children);
+    }
+    if (!Array.isArray(childs)) {
+      childs = [childs];
+    }
+    const slotted = Array.from(childs).find((child) => {
+      return isValidElement(child) && child.tag === Slot;
+    });
+
+    if (!slotted || !isValidElement(slotted)) {
+      return raw(children);
+    }
+    return raw(
+      cloneElement(slotted, {
+        restProps: { ...restProps, defaultChildren: children },
+        showChildren: true,
+      } as unknown as SlotProps<P>),
+    );
+  };
+  Slot.Renderer = Renderer;
+  return Slot;
+}
---
+++ packages/hono-slotify/src/indext.test.tsx
@@ -0,0 +1,126 @@
+//
+
+//
+// Inspired by https://github.com/andrey-skl/react-slotify/blob/6317a858696544c4d17db3d1823625beece3412c/test/slot.test.tsx
+//
+
+import { expect, test } from "bun:test";
+import type { FC, PropsWithChildren } from "hono/jsx";
+import { createSlot } from ".";
+
+test("render basic slot", () => {
+  const TestSlot = createSlot();
+  const Component: FC<PropsWithChildren> = ({ children }) => {
+    return (
+      <div>
+        beforeslot <TestSlot.Renderer childs={children} /> afterslot;
+        ownChildren: {children}
+      </div>
+    );
+  };
+  expect(
+    (
+      <div>
+        beforecomponent
+        <Component>
+          BEFORE_SLOT<TestSlot>IN_SLOT</TestSlot>AFTER_SLOT
+        </Component>
+        aftercomponent
+      </div>
+    ).toString(),
+  ).toMatchSnapshot();
+});
+
+test("eender undefined childs", () => {
+  const TestSlot = createSlot();
+  const Component: FC<PropsWithChildren> = ({ children }) => {
+    return (
+      <div>
+        beforeslot <TestSlot.Renderer childs={undefined} /> afterslot;
+        ownChildren: {children}
+      </div>
+    );
+  };
+  expect(
+    (
+      <div>
+        beforecomponent
+        <Component>
+          BEFORE_SLOT<TestSlot>IN_SLOT</TestSlot>AFTER_SLOT
+        </Component>
+        aftercomponent
+      </div>
+    ).toString(),
+  ).toMatchSnapshot();
+});
+
+test("render default content if slot is not used", () => {
+  const TestSlot = createSlot();
+
+  const Component: FC<PropsWithChildren> = ({ children }) => {
+    return (
+      <pre>
+        beforeslot
+        <TestSlot.Renderer childs={children}>
+          DEFAULT_SLOT_CONTENT
+        </TestSlot.Renderer>
+        afterslot; ownChildren: {children}
+      </pre>
+    );
+  };
+  expect(
+    (
+      <main>
+        beforecomponent
+        <Component>Slot not used</Component>
+        aftercomponent
+      </main>
+    ).toString(),
+  ).toMatchSnapshot();
+});
+
+test("pass parameters", () => {
+  const TestSlot = createSlot<{ foo: string }>();
+
+  const Component: FC<PropsWithChildren> = ({ children }) => {
+    return (
+      <pre>
+        <TestSlot.Renderer childs={children} foo="bar" />
+      </pre>
+    );
+  };
+  expect(
+    (
+      <main>
+        <Component>
+          <TestSlot>{(params) => <div>foo={params.foo}</div>}</TestSlot>
+        </Component>
+      </main>
+    ).toString(),
+  ).toMatchSnapshot();
+});
+
+test("pass default children to function", () => {
+  const TestSlot = createSlot();
+
+  const Component: FC<PropsWithChildren> = ({ children }) => {
+    return (
+      <pre>
+        <TestSlot.Renderer childs={children}>
+          DEFAULT_SLOT_CONTENT
+        </TestSlot.Renderer>
+      </pre>
+    );
+  };
+  expect(
+    (
+      <main>
+        <Component>
+          <TestSlot>
+            {(params) => <div>foo={params.defaultChildren}</div>}
+          </TestSlot>
+        </Component>
+      </main>
+    ).toString(),
+  ).toMatchSnapshot();
+});
