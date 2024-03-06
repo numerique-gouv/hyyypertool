a27d13a040f23af9f98c0b473fc8761a2a46741b
Douglas DUTEIL <douglasduteil@gmail.com>
2024-03-06 10:05:46 +0100

    add visually_hidden

---
+++ packages/~/app/ui/src/visually_hidden/index.ts
@@ -0,0 +1,18 @@
+//
+
+import { tv } from "tailwind-variants";
+
+//
+export const visually_hidden = tv({
+  base: `
+    !absolute
+    !-m-px
+    !h-px
+    !w-px
+    !overflow-hidden
+    !whitespace-nowrap
+    !border-0
+    !p-0
+    ![clip:rect(0,0,0,0)]
+  `,
+});
