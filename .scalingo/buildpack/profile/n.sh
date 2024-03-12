a4b1b91b13baf1bc2049efeb211bb994168cef69
Douglas DUTEIL <douglasduteil@gmail.com>
2024-03-12 20:53:12 +0100

    chore(scalingo): add Scalingo CA_CERTS

--- .scalingo/buildpack/profile/n.sh
+++ .scalingo/buildpack/profile/n.sh
@@ -3,3 +3,4 @@
 export N_PREFIX="$HOME/.scalingo/n"
 export PATH="$N_PREFIX/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_ENV=${NODE_ENV:-production}
+export NODE_EXTRA_CA_CERTS=${NODE_EXTRA_CA_CERTS:-/usr/share/ca-certificates/Scalingo/scalingo-database.pem}
