f30393099a8f46f5e79df316774642b92b06eeb6
Douglas DUTEIL <douglasduteil@gmail.com>
2024-03-15 13:59:06 +0100

    fix(app): do not change header of finalized context

--- packages/~/app/middleware/src/cache_immutable.ts
+++ packages/~/app/middleware/src/cache_immutable.ts
@@ -6,8 +6,9 @@ import { createMiddleware } from "hono/factory";
 //
 
 export const cache_immutable = createMiddleware(
-  async function immutable_middleware({ header }, next) {
+  async function immutable_middleware({ finalized, header }, next) {
     await next();
+    if (finalized) return;
     if (config.NODE_ENV === "development") return;
     header("cache-control", "public,max-age=31536000,immutable");
   },
