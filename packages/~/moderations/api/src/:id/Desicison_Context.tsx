b0845c604bdf246f457d323a5e26cee0c95992c9
Douglas DUTEIL <douglasduteil@gmail.com>
2024-03-20 08:53:57 +0100

    chore(deps): update dependencies

--- packages/~/moderations/api/src/:id/Desicison_Context.tsx
+++ packages/~/moderations/api/src/:id/Desicison_Context.tsx
@@ -1,12 +1,15 @@
 //
 
 import { hyper_ref } from "@~/app.core/html";
+import { urls } from "@~/app.urls";
+import type { InferRequestType } from "hono";
 import { createContext } from "hono/jsx";
 
 //
 export const Desicison_Context = createContext({
   $accept: hyper_ref(),
-  $allow: hyper_ref(),
+  $add_as_internal_member: hyper_ref(),
+  $add_domain: hyper_ref(),
   $destination: hyper_ref(),
   $form: hyper_ref(),
   $message: hyper_ref(),
@@ -14,3 +17,10 @@ export const Desicison_Context = createContext({
   $reject: hyper_ref(),
   $select: hyper_ref(),
 });
+
+//
+
+const validation_mutation = urls.moderations[":id"].$procedures.validate.$patch;
+export type Validation_MutationInputKeys = keyof InferRequestType<
+  typeof validation_mutation
+>["form"];
--- packages/~/moderations/api/src/:id/Member_Valid.tsx
+++ packages/~/moderations/api/src/:id/Member_Valid.tsx
@@ -3,20 +3,21 @@
 import { Htmx_Events, hx_include } from "@~/app.core/htmx";
 import { button } from "@~/app.ui/button";
 import { fieldset } from "@~/app.ui/form";
-import { hx_urls, urls } from "@~/app.urls";
-import type { InferRequestType } from "hono";
+import { hx_urls } from "@~/app.urls";
 import { useContext } from "hono/jsx";
-import { Desicison_Context } from "./Desicison_Context";
+import {
+  Desicison_Context,
+  type Validation_MutationInputKeys,
+} from "./Desicison_Context";
 import { ModerationPage_Context } from "./context";
 
 //
 
 export function Member_Valid() {
-  const { $accept, $allow, $form } = useContext(Desicison_Context);
-  const { domain, moderation } = useContext(ModerationPage_Context);
+  const { $accept, $add_domain, $form } = useContext(Desicison_Context);
+  const { moderation } = useContext(ModerationPage_Context);
   const { base, element } = fieldset();
-  const $patch = urls.moderations[":id"].$procedures.validate.$patch;
-  type FormNames = keyof InferRequestType<typeof $patch>["form"];
+
   return (
     <form
       _={`
@@ -33,25 +34,17 @@ export function Member_Valid() {
       {...hx_urls.moderations[":id"].$procedures.validate.$patch({
         param: { id: moderation.id.toString() },
       })}
-      hx-include={hx_include([$allow])}
+      hx-include={hx_include([$add_domain])}
     >
       <fieldset class={base()}>
         <div class={element()}>
-          <div class="fr-checkbox-group">
-            <input
-              _="
-              on click set @value to my checked
-              "
-              id={$allow}
-              name={"add_domain" as FormNames}
-              type="checkbox"
-              value="true"
-            />
-            <label class="fr-label" for={$allow}>
-              J’autorise le domaine <b class="mx-1">{domain}</b> pour toute
-              l’organisation
-            </label>
-          </div>
+          <AddDomain />
+        </div>
+        <div class={element()}>
+          <DoNotAddMember />
+        </div>
+        <div class={element()}>
+          <AddAsMember />
         </div>
         <div class={element({ class: "mt-8" })}>
           <button class={button()} type="submit">
@@ -60,5 +53,75 @@ export function Member_Valid() {
         </div>
       </fieldset>
     </form>
+  );
+}
+
+function AddDomain() {
+  const { $add_domain } = useContext(Desicison_Context);
+  const { domain } = useContext(ModerationPage_Context);
+
+  return (
+    <div class="fr-checkbox-group">
+      <input
+        _="on click set @value to my checked"
+        id={$add_domain}
+        name={"add_domain" as Validation_MutationInputKeys}
+        type="checkbox"
+        value="true"
+      />
+      <label class="fr-label !flex-row" for={$add_domain}>
+        J’autorise le domaine <b class="mx-1">{domain}</b> pour toute
+        l’organisation
+      </label>
+    </div>
+  );
+}
+
+function DoNotAddMember() {
+  const { $add_as_internal_member } = useContext(Desicison_Context);
+  const {
+    moderation: {
+      users: { given_name },
+    },
+  } = useContext(ModerationPage_Context);
+
+  return (
+    <div class="fr-radio-group">
+      <input
+        _="on click set @value to my checked"
+        id={$add_as_internal_member}
+        name={"add_domain" as Validation_MutationInputKeys}
+        type="radio"
+        value="false"
+      />
+      <label class="fr-label !flex-row" for={$add_as_internal_member}>
+        Ne pas ajouter <b class="mx-1">{given_name}</b> à l'organisation
+      </label>
+    </div>
+  );
+}
+
+function AddAsMember() {
+  const { $add_as_internal_member } = useContext(Desicison_Context);
+  const {
+    moderation: {
+      users: { given_name },
+    },
+  } = useContext(ModerationPage_Context);
+
+  return (
+    <div class="fr-radio-group">
+      <input
+        _="on click set @value to my checked"
+        id={$add_as_internal_member}
+        name={"add_domain" as Validation_MutationInputKeys}
+        type="radio"
+        value="true"
+      />
+      <label class="fr-label !flex-row" for={$add_as_internal_member}>
+        Ajouter <b class="mx-1">{given_name}</b> à l'organisation EN TANT
+        QU'INTERNE
+      </label>
+    </div>
   );
 }
