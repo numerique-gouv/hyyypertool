2b4fa7793749c6d822b2e88a79110698dbbd0451
Douglas DUTEIL <douglasduteil@gmail.com>
2024-03-26 10:32:58 +0100

    use zod to list the types

--- packages/~/moderations/lib/src/Moderation_Type.ts
+++ packages/~/moderations/lib/src/Moderation_Type.ts
@@ -4,15 +4,11 @@ import { z } from "zod";
 
 //
 
-export type Moderation_Type =
-  | "ask_for_sponsorship"
-  | "big_organization_join"
-  | "non_verified_domain"
-  | "organization_join_block";
-
 export const Moderation_Type_Schema = z.enum([
   "ask_for_sponsorship",
   "big_organization_join",
   "non_verified_domain",
   "organization_join_block",
 ]);
+
+export type Moderation_Type = z.TypeOf<typeof Moderation_Type_Schema>;
