1560ff23fcd6b6f57cfb64991a05e92df5b244e6
Douglas DUTEIL <douglasduteil@gmail.com>
2024-03-27 18:19:37 +0100

    fix(moderations): force autoreload on load

--- packages/~/moderations/api/src/page.tsx
+++ packages/~/moderations/api/src/page.tsx
@@ -87,6 +87,7 @@ export function Moderations_Page({
         {...hx_moderations_query_props}
         hx-sync="this"
         hx-trigger={[
+          `load`,
           `every 11s [document.visibilityState === 'visible']`,
           `visibilitychange[document.visibilityState === 'visible'] from:document`,
         ].join(", ")}
