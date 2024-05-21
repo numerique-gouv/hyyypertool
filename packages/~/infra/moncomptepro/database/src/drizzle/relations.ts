0a955f084f792b58f3d0877f86c8e87bd37f7e81
Douglas DUTEIL <douglasduteil@gmail.com>
2024-05-21 11:08:55 +0200

    feat(moncomptepro): update database schema

---
+++ packages/~/infra/moncomptepro/database/src/drizzle/relations.ts
@@ -0,0 +1,79 @@
+import { relations } from "drizzle-orm/relations";
+import {
+  organizations,
+  moderations,
+  users,
+  oidc_clients,
+  users_oidc_clients,
+  users_organizations,
+} from "./schema";
+
+export const moderationsRelations = relations(moderations, ({ one }) => ({
+  organization: one(organizations, {
+    fields: [moderations.organization_id],
+    references: [organizations.id],
+  }),
+  user: one(users, {
+    fields: [moderations.user_id],
+    references: [users.id],
+  }),
+}));
+
+export const organizationsRelations = relations(organizations, ({ many }) => ({
+  moderations: many(moderations),
+  users_oidc_clients: many(users_oidc_clients),
+  users_organizations: many(users_organizations),
+}));
+
+export const usersRelations = relations(users, ({ many }) => ({
+  moderations: many(moderations),
+  users_oidc_clients: many(users_oidc_clients),
+  users_organizations_sponsor_id: many(users_organizations, {
+    relationName: "users_organizations_sponsor_id_users_id",
+  }),
+  users_organizations_user_id: many(users_organizations, {
+    relationName: "users_organizations_user_id_users_id",
+  }),
+}));
+
+export const users_oidc_clientsRelations = relations(
+  users_oidc_clients,
+  ({ one }) => ({
+    oidc_client: one(oidc_clients, {
+      fields: [users_oidc_clients.oidc_client_id],
+      references: [oidc_clients.id],
+    }),
+    organization: one(organizations, {
+      fields: [users_oidc_clients.organization_id],
+      references: [organizations.id],
+    }),
+    user: one(users, {
+      fields: [users_oidc_clients.user_id],
+      references: [users.id],
+    }),
+  }),
+);
+
+export const oidc_clientsRelations = relations(oidc_clients, ({ many }) => ({
+  users_oidc_clients: many(users_oidc_clients),
+}));
+
+export const users_organizationsRelations = relations(
+  users_organizations,
+  ({ one }) => ({
+    organization: one(organizations, {
+      fields: [users_organizations.organization_id],
+      references: [organizations.id],
+    }),
+    user_sponsor_id: one(users, {
+      fields: [users_organizations.sponsor_id],
+      references: [users.id],
+      relationName: "users_organizations_sponsor_id_users_id",
+    }),
+    user_user_id: one(users, {
+      fields: [users_organizations.user_id],
+      references: [users.id],
+      relationName: "users_organizations_user_id_users_id",
+    }),
+  }),
+);
