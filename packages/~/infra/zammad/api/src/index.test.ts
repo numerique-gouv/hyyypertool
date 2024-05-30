1b4f5f3847eea0aa639e11e655e81b2a375c6efe
Douglas Duteil <douglasduteil@gmail.com>
2024-05-30 11:17:48 +0200

    fix(zammad): no more immutable errors (#294)

---
+++ packages/~/infra/zammad/api/src/index.test.ts
@@ -0,0 +1,73 @@
+//
+
+import type { get_zammad_attachment } from "@~/zammad.lib";
+import { expect, mock, test } from "bun:test";
+
+//
+
+test("GET /attachment/123/456/789 : 🪴", async () => {
+  mock.module("@~/zammad.lib", () => {
+    return {
+      get_zammad_attachment: mock<typeof get_zammad_attachment>(() =>
+        Promise.resolve(new Response("🪴")),
+      ),
+    };
+  });
+
+  const { default: app } = await import("./index");
+
+  const res = await app.request("/attachment/123/456/789");
+
+  expect(res.status).toBe(200);
+  expect(await res.text()).toEqual("🪴");
+});
+
+test("❎ fails with 404 on TypeError", async () => {
+  mock.module("@~/zammad.lib", () => {
+    return {
+      get_zammad_attachment: mock<typeof get_zammad_attachment>(() =>
+        Promise.reject(new TypeError("🧟")),
+      ),
+    };
+  });
+
+  const { default: app } = await import("./index");
+
+  const res = await app.request("/attachment/123/456/789");
+
+  expect(res.status).toBe(404);
+  expect(await res.text()).toEqual("404 Not Found");
+});
+
+test("❎ fails with 404 on SyntaxError", async () => {
+  mock.module("@~/zammad.lib", () => {
+    return {
+      get_zammad_attachment: mock<typeof get_zammad_attachment>(() =>
+        Promise.reject(new SyntaxError("🧟")),
+      ),
+    };
+  });
+
+  const { default: app } = await import("./index");
+
+  const res = await app.request("/attachment/123/456/789");
+
+  expect(res.status).toBe(404);
+  expect(await res.text()).toEqual("404 Not Found");
+});
+
+test("❌ bubble other errors", async () => {
+  mock.module("@~/zammad.lib", () => {
+    return {
+      get_zammad_attachment: mock<typeof get_zammad_attachment>(() =>
+        Promise.reject(new Error("🦭")),
+      ),
+    };
+  });
+
+  const { default: app } = await import("./index");
+
+  expect(async () => {
+    await app.request("/attachment/123/456/789");
+  }).toThrow("🦭");
+});
