99c9605eb0b062dd3ec2163160575d7de159c277
Douglas Duteil <douglasduteil@gmail.com>
2024-07-17 16:17:51 +0200

    feat(organisations): add no_validation_means_available (#400)
    Closes #21
    Closes #378

--- packages/~/infra/moncomptepro/lib/src/verification_type.ts
+++ packages/~/infra/moncomptepro/lib/src/verification_type.ts
@@ -7,6 +7,7 @@ import type { MCP_UserOrganizationLink } from "./moncomptepro";
 export const Verification_Type_Schema = z.enum([
   "code_sent_to_official_contact_email",
   "in_liste_dirigeants_rna",
+  "no_validation_means_available",
   "official_contact_domain",
   "official_contact_email",
   "trackdechets_email_domain",
