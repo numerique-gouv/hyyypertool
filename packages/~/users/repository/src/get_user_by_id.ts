99725779ad6c2604bc9efe0f44158bff136b364e
Douglas DUTEIL <douglasduteil@gmail.com>
2024-07-18 13:30:43 +0200

    perf(user): exact column user query

--- packages/~/users/repository/src/get_user_by_id.ts
+++ packages/~/users/repository/src/get_user_by_id.ts
@@ -10,6 +10,21 @@ export async function get_user_by_id(
   { id }: { id: number },
 ) {
   return pg.query.users.findFirst({
+    columns: {
+      created_at: true,
+      email_verified: true,
+      email: true,
+      family_name: true,
+      given_name: true,
+      id: true,
+      job: true,
+      last_sign_in_at: true,
+      phone_number: true,
+      reset_password_sent_at: true,
+      sign_in_count: true,
+      updated_at: true,
+      verify_email_sent_at: true,
+    },
     where: eq(schema.users.id, id),
   });
 }
