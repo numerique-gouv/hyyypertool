51799f49c7c543b841bfc1481ba0773dd575261a
Douglas DUTEIL <douglasduteil@gmail.com>
2024-07-18 13:10:24 +0200

    refacto(users): use page request context

--- packages/~/users/api/src/index.tsx
+++ packages/~/users/api/src/index.tsx
@@ -1,40 +1,33 @@
 //
 
 import { zValidator } from "@hono/zod-validator";
-import { Entity_Schema, Pagination_Schema } from "@~/app.core/schema";
 import { Main_Layout } from "@~/app.layout/index";
-import type { App_Context } from "@~/app.middleware/context";
+import { urls } from "@~/app.urls";
+import consola from "consola";
 import { Hono } from "hono";
 import { jsxRenderer } from "hono/jsx-renderer";
-import { z } from "zod";
 import user_id_router from "./:id";
-import UsersPage, { SEARCH_EMAIL_INPUT_ID } from "./page";
+import { get_users_list, PageInput_Schema, type ContextType } from "./context";
+import Page from "./page";
 
 //
 
-export default new Hono<App_Context>()
+export default new Hono<ContextType>()
   .route("/:id", user_id_router)
   //
   .use("/", jsxRenderer(Main_Layout))
   .get(
     "/",
-    zValidator(
-      "query",
-      Pagination_Schema.extend({
-        [SEARCH_EMAIL_INPUT_ID]: z.string().optional(),
-      }).merge(Entity_Schema.partial()),
-    ),
-    function ({ render, req }) {
-      const { page, [SEARCH_EMAIL_INPUT_ID]: email } = req.valid("query");
-
-      return render(
-        <UsersPage
-          pagination={{
-            page: page,
-            page_size: 10,
-          }}
-          email={email}
-        />,
-      );
+    ({ set }, next) => {
+      set("query_users", get_users_list);
+      return next();
+    },
+    zValidator("query", PageInput_Schema, function hook(result, { redirect }) {
+      if (result.success) return undefined;
+      consola.error(result.error);
+      return redirect(urls.users.$url().pathname);
+    }),
+    async function GET({ render }) {
+      return render(<Page />);
     },
   );
--- packages/~/users/api/src/page.tsx
+++ packages/~/users/api/src/page.tsx
@@ -1,58 +1,158 @@
 //
 
-import type { Pagination } from "@~/app.core/schema";
-import type { MonComptePro_Pg_Context } from "@~/app.middleware/moncomptepro_pg";
-import { hx_urls } from "@~/app.urls";
-import { useRequestContext } from "hono/jsx-renderer";
-import { Table } from "./Table";
-import { Table_Context, get_users_list } from "./context";
+import { hyper_ref } from "@~/app.core/html";
+import { hx_include } from "@~/app.core/htmx";
+import { Pagination_Schema } from "@~/app.core/schema";
+import { Foot } from "@~/app.ui/hx_table";
+import { row } from "@~/app.ui/table";
+import { LocalTime } from "@~/app.ui/time/LocalTime";
+import { hx_urls, urls } from "@~/app.urls";
+import { match } from "ts-pattern";
+import {
+  PageInput_Schema,
+  usePageRequestContext,
+  type get_users_list_dto,
+} from "./context";
 
 //
 
-export const SEARCH_EMAIL_INPUT_ID = "search-email";
-export const USER_TABLE_ID = "user-table";
+const $search = hyper_ref();
+const $table = hyper_ref();
+const hx_users_list_query_props = {
+  ...(await hx_urls.users.$get({ query: {} })),
+  "hx-include": hx_include([
+    $table,
+    $search,
+    PageInput_Schema.keyof().enum.page,
+  ]),
+  "hx-replace-url": true,
+  "hx-select": `#${$table} > table`,
+  "hx-target": `#${$table}`,
+};
 
 //
 
-//
-
-export default async function Page({
-  pagination,
-  email,
-}: {
-  email: string | undefined;
-  pagination: Pagination;
-}) {
-  const {
-    var: { moncomptepro_pg },
-  } = useRequestContext<MonComptePro_Pg_Context>();
-  const { page, page_size } = pagination;
-  const { count, users } = await get_users_list(moncomptepro_pg, {
-    search: { email },
-    pagination: { page: page - 1, take: page_size },
-  });
+export default async function Page() {
   return (
-    <main class="fr-container">
+    <main class="fr-container my-12">
       <h1>Liste des utilisateurs</h1>
-      <label class="fr-label" for={SEARCH_EMAIL_INPUT_ID}>
-        Email
-      </label>
-      <input
-        class="fr-input"
-        {...await hx_urls.users.$get({ query: {} })}
-        hx-select={`#${USER_TABLE_ID} > table`}
-        hx-target={`#${USER_TABLE_ID}`}
-        hx-trigger="input changed delay:500ms, search"
-        id={SEARCH_EMAIL_INPUT_ID}
-        name={SEARCH_EMAIL_INPUT_ID}
-        placeholder="Recherche par Email"
-        type="email"
-      />
-      <Table_Context.Provider value={{ page, take: page_size, count }}>
-        <div class="fr-table" id={USER_TABLE_ID}>
-          <Table users={users} />
-        </div>
-      </Table_Context.Provider>
+      <Filter />
+      <Table />
     </main>
+  );
+}
+
+//
+
+function Filter() {
+  const { req } = usePageRequestContext();
+  const { q } = req.valid("query");
+  return (
+    <form
+      {...hx_users_list_query_props}
+      hx-trigger={[`keyup changed delay:500ms from:#${$search}`].join(", ")}
+    >
+      <div class="fr-search-bar" role="search">
+        <label class="fr-label" for={$search}>
+          Recherche
+        </label>
+        <input
+          class="fr-input"
+          id={$search}
+          name={PageInput_Schema.keyof().enum.q}
+          placeholder="Recherche"
+          value={q}
+          type="search"
+        />
+        <button class="fr-btn" title="Rechercher">
+          Rechercher
+        </button>
+      </div>
+    </form>
+  );
+}
+
+async function Table() {
+  const {
+    req,
+    var: { query_users, moncomptepro_pg },
+  } = usePageRequestContext();
+
+  const { q } = req.valid("query");
+  const pagination = match(
+    Pagination_Schema.safeParse(req.query(), { path: ["req.query()"] }),
+  )
+    .with({ success: true }, ({ data }) => data)
+    .otherwise(() => Pagination_Schema.parse({}));
+
+  const { count, users } = await query_users(moncomptepro_pg, {
+    search: q ? String(q) : undefined,
+    pagination: { ...pagination, page: pagination.page - 1 },
+  });
+
+  return (
+    <div class="fr-table [&>table]:table" id={$table}>
+      <table>
+        <thead>
+          <tr>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Email</th>
+            <th>Date de création</th>
+            <th>Dernière connexion</th>
+            <th>Email vérifié le</th>
+            <th>ID</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <Row key={`${user.id}`} user={user} />
+          ))}
+        </tbody>
+        <Foot
+          count={count}
+          hx_query_props={hx_users_list_query_props}
+          id={$table}
+          name={PageInput_Schema.keyof().enum.page}
+          pagination={pagination}
+        />
+      </table>
+    </div>
+  );
+}
+
+function Row({
+  key,
+  user,
+}: {
+  key?: string;
+  user: get_users_list_dto["users"][number];
+}) {
+  return (
+    <tr
+      _={`on click set the window's location to '${
+        urls.users[":id"].$url({
+          param: { id: user.id.toString() },
+        }).pathname
+      }'`}
+      class={row({ is_clickable: true })}
+      key={key}
+    >
+      <td>{user.given_name}</td>
+      <td>{user.family_name}</td>
+      <td>{user.email}</td>
+      <td>
+        <LocalTime date={user.created_at} />
+      </td>
+      <td>
+        <LocalTime date={user.last_sign_in_at} />
+      </td>
+      <td>
+        <LocalTime date={user.email_verified_at} />
+      </td>
+      <td>{user.id}</td>
+      <td class="!text-right">➡️</td>
+    </tr>
   );
 }
