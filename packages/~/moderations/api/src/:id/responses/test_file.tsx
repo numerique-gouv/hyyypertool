73a6ce4b837b6f81e9a57332c2abb369aec91b46
Rebecca Dumazert <44778533+rebeccadumazert@users.noreply.github.com>
2024-09-12 00:28:10 +0200

    Create test_file.tsx

---
+++ packages/~/moderations/api/src/:id/responses/test_file.tsx
@@ -0,0 +1,28 @@
+//
+
+import { dedent } from "ts-dedent";
+import { usePageRequestContext } from "../context";
+
+export const label = "Votre titre de mail";
+
+export default function template() {
+  const {
+    var: {
+      moderation: {
+        organization: { cached_libelle: organization_name, siret },
+      },
+    },
+  } = usePageRequestContext();
+
+  return dedent`
+    Bonjour,
+
+    Votre demande pour rejoindre l'organisation « ${organization_name} » a été prise en compte sur https://app.moncomptepro.beta.gouv.fr.
+
+    Vous disposez d’une adresse mail académique (educagri.fr), à ce titre, vous n’êtes pas autorisé à rattacher votre compte au Ministère de l’Agriculture.
+    En fonction de votre profession et de votre périmètre d’intervention, merci de bien vouloir renouveler votre inscription en rattachant votre compte à l'établissement dans lequel vous exercez,
+
+    Excellente journée,
+    L’équipe MonComptePro.
+  `;
+}
