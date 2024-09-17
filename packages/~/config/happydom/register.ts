e078dc85d4e531bc578ca2544f2837de94157dcc
Douglas DUTEIL <douglasduteil@gmail.com>
2024-09-17 11:24:41 +0200

    chore(config): remove navigator hack

--- packages/~/config/happydom/register.ts
+++ packages/~/config/happydom/register.ts
@@ -2,11 +2,6 @@
 
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
 
-// NOTE(douglasduteil): delete global.navigator
-// Ensure that the global.navigator is not defined to avoid conflicts with the
-// Navigator object from the browser.
-delete (global as any)["navigator"];
-
 //
 
 GlobalRegistrator.register();
