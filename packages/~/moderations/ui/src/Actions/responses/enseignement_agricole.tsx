c72dd88d2ce6c9a71e61a3014d5f6781de6d12d2
Douglas Duteil <douglasduteil@gmail.com>
2024-11-12 17:55:31 +0100

    feat(moderation): add enseignant agricole response (#630)

---
+++ packages/~/moderations/ui/src/Actions/responses/enseignement_agricole.tsx
@@ -0,0 +1,29 @@
+//
+
+import { useContext } from "hono/jsx";
+import { dedent } from "ts-dedent";
+import { context } from "../context";
+
+export const label = "Enseignant e-mail educagri.fr —> Min de l’Agriculture";
+
+export default function template() {
+  const {
+    domain,
+    moderation: {
+      organization: { cached_libelle: organization_name },
+    },
+  } = useContext(context);
+
+  return dedent`
+    Bonjour,
+
+    Nous avons bien reçu votre demande de rattachement à l'organisation « ${organization_name} » sur ProConnect (anciennement : AgentConnect, MonComptePro).
+
+    Votre adresse e-mail d’enseignement agricole : « ${domain} » ne vous permet pas de rattacher votre compte utilisateur ProConnect au « ${organization_name} ».
+
+    En tant qu'enseignant, merci de bien vouloir renouveler votre inscription en rattachant votre compte à l'**Établissement Public Local d'Enseignement et de Formation Professionnelle Agricoles** (EPLEFPA) dans lequel vous exercez.
+
+    Bien cordialement,
+    L’équipe ProConnect.
+  `;
+}
