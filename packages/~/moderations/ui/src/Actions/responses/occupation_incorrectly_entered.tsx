0bedd9c5a71a52c03381cd66d3dfa383278594c6
rebeccadumazert <rebeccadumazert@hotmail.fr>
2024-11-14 17:42:23 +0100

    feat(moderation): add  occupation incorrectly entered response

---
+++ packages/~/moderations/ui/src/Actions/responses/occupation_incorrectly_entered.tsx
@@ -0,0 +1,36 @@
+//
+
+import { useContext } from "hono/jsx";
+import { dedent } from "ts-dedent";
+import { context } from "../context";
+
+export const label = "Profession mal renseignée";
+
+export default function template() {
+  const {
+    moderation: {
+      organization: { cached_libelle: organization_name, siret },
+    },
+  } = useContext(context);
+
+  return dedent`
+    Bonjour,
+
+    Nous avons bien reçu votre demande de rattachement à l'organisation « ${organization_name} » sur ProConnect (anciennement : AgentConnect, MonComptePro).
+
+    Afin que votre demande puisse être validée, merci de renseigner correctement votre profession sur le champ « Profession ou rôle au sein de votre organisation ». 
+    Votre profession ne peut pas être le nom de votre organisation ou le nom d’un service de votre organisation.
+
+    Pour se faire :
+
+    - Videz le cache de votre navigateur puis rafraîchissez votre page internet, (lien)
+    - Accédez à MonComptePro : https://app.moncomptepro.beta.gouv.fr/, puis authentifiez-vous,
+    - Cliquez sur l'onglet « Vos Informations Personnelles »,
+    - Corrigez votre profession,
+    - Sélectionnez votre organisation (numéro SIRET : ${siret}) pour que notre équipe puisse valider à nouveau la création de votre compte.
+
+    Merci pour votre correction.
+    Bien cordialement,
+    L’équipe ProConnect.
+  `;
+}
