77e86e6ed5ccefd6d087ddd0f1a60fd161298430
Douglas DUTEIL <douglasduteil@gmail.com>
2024-11-19 15:45:11 +0100

    refactor(ui): extract hide on click elsewere script

---
+++ packages/~/app/ui/src/menu/components/scripts/hide_on_click_elsewere.test.tsx
@@ -0,0 +1,27 @@
+//
+
+import { expect, test } from "bun:test";
+import "htmx.org";
+import _hyperscript from "hyperscript.org";
+import "hyperscript.org/src/hdb";
+import { hide_on_click_elsewere } from "./hide_on_click_elsewere";
+
+//
+
+test("hide on click elsewere", () => {
+  document.body.innerHTML = (
+    <>
+      <div _={hide_on_click_elsewere("#ike")}></div>
+      <div id="ike"></div>
+    </>
+  ).toString();
+
+  _hyperscript.processNode(document.body);
+
+  const ike = document.querySelector("#ike")!;
+
+  expect(ike.hasAttribute("hidden")).toBeFalse();
+  document.body.click();
+
+  expect(ike.hasAttribute("hidden")).toBeTrue();
+});
