dfe2d0a6a44798a2d41a7a99b5592f06446d33ff
Douglas DUTEIL <douglasduteil@gmail.com>
2024-12-04 15:32:43 +0100

    feat(organization): add verified_by_coop_mediation_numerique

--- packages/~/organizations/api/src/:id/members/Table.tsx
+++ packages/~/organizations/api/src/:id/members/Table.tsx
@@ -204,6 +204,29 @@ async function Row_Actions() {
                     user_id: user_id.toString(),
                   },
                   form: {
+                    verification_type: "verified_by_coop_mediation_numerique",
+                  },
+                },
+              )}
+              hx-swap="none"
+            >
+              🔄 vérif: no verified by coop mediation numerique
+            </button>
+          ) : (
+            <></>
+          )}
+        </li>
+        <li>
+          {verification_type ? (
+            <button
+              class={menu_item()}
+              {...await hx_urls.organizations[":id"].members[":user_id"].$patch(
+                {
+                  param: {
+                    id: organization_id.toString(),
+                    user_id: user_id.toString(),
+                  },
+                  form: {
                     verification_type: "",
                   },
                 },
