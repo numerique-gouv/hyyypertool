e3c6aa15f4888cd35174a8d1082c5dbcffcc4e92
Douglas DUTEIL <douglasduteil@gmail.com>
2025-01-02 14:59:07 +0100

    feat(ui): expect navigator.writeText to exist

--- packages/~/app/ui/src/button/scripts/copy_text_content_to_clipboard.ts
+++ packages/~/app/ui/src/button/scripts/copy_text_content_to_clipboard.ts
@@ -4,10 +4,8 @@ export function copy_text_content_to_clipboard(element: `#${string}` | string) {
   return `
     on click
       set text to ${element}.textContent
-      js(me, text)
-        if ('clipboard' in window.navigator) {
-          navigator.clipboard.writeText(text.trim())
-        }
+      js(text)
+        navigator.clipboard.writeText(text.trim())
       end
       halt
   `;
