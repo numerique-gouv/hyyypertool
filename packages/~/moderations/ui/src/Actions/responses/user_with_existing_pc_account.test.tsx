24355a7cd99b899f10041c1f220889b3ddf5f170
Douglas Duteil <douglasduteil@gmail.com>
2025-02-03 09:52:45 +0100

    fix(moderation): existing email response (#768)

--- packages/~/moderations/ui/src/Actions/responses/user_with_existing_pc_account.test.tsx
+++ packages/~/moderations/ui/src/Actions/responses/user_with_existing_pc_account.test.tsx
@@ -1,13 +1,17 @@
 //
 
 import { render_md } from "@~/app.ui/testing";
+import type { SuggestSameUserEmailsHandler } from "@~/users.lib/usecase/SuggestSameUserEmails";
 import { expect, test } from "bun:test";
 import { context, type Values } from "../context";
 import user_with_existing_pc_account from "./user_with_existing_pc_account";
 
 //
 
-test("returns user with existing pc account", async () => {
+test("with one email found", async () => {
+  const query_suggest_same_user_emails: SuggestSameUserEmailsHandler =
+    async () => ["🦄@unicorn.xyz"];
+
   expect(
     await render_md(
       <context.Provider
@@ -18,13 +22,108 @@ test("returns user with existing pc account", async () => {
               organization: { cached_libelle: "🦄" },
               user: { email: "💌" },
             },
+            query_suggest_same_user_emails,
           } as Values
         }
       >
         <Response />
       </context.Provider>,
     ),
-  ).toMatchSnapshot();
+  ).toMatchInlineSnapshot(`
+    "Bonjour,
+
+    Nous avons bien reçu votre demande de rattachement à l&#39;organisation « 🦄 » sur ProConnect (anciennement : AgentConnect, MonComptePro).
+
+    Vous possédez déjà un compte ProConnect associé à l’adresse e-mail professionnelle : « 🦄@unicorn.xyz ».
+
+    Merci de bien vouloir vous connecter avec le compte déjà existant.
+
+    Votre adresse e-mail associée à un nom de domaine gratuit tel que « 🧨 » ne sera pas autorisée.
+
+    Bien cordialement,
+    L’équipe ProConnect.
+    "
+  `);
+});
+
+test("with three emails found", async () => {
+  const query_suggest_same_user_emails: SuggestSameUserEmailsHandler =
+    async () => ["🦄@unicorn.xyz", "🐷@unicorn.xyz", "🧧@unicorn.xyz"];
+
+  expect(
+    await render_md(
+      <context.Provider
+        value={
+          {
+            domain: "🧨",
+            moderation: {
+              organization: { cached_libelle: "🦄" },
+              user: { email: "💌" },
+            },
+            query_suggest_same_user_emails,
+          } as Values
+        }
+      >
+        <Response />
+      </context.Provider>,
+    ),
+  ).toMatchInlineSnapshot(`
+    "Bonjour,
+
+    Nous avons bien reçu votre demande de rattachement à l&#39;organisation « 🦄 » sur ProConnect (anciennement : AgentConnect, MonComptePro).
+
+    Vous possédez déjà un compte ProConnect associé à l’adresse e-mail professionnelle :
+
+    - 🦄@unicorn.xyz
+    - 🐷@unicorn.xyz
+    - 🧧@unicorn.xyz
+
+    Merci de bien vouloir vous connecter avec le compte déjà existant.
+
+    Votre adresse e-mail associée à un nom de domaine gratuit tel que « 🧨 » ne sera pas autorisée.
+
+    Bien cordialement,
+    L’équipe ProConnect.
+    "
+  `);
+});
+
+test("with no emails found", async () => {
+  const query_suggest_same_user_emails: SuggestSameUserEmailsHandler =
+    async () => [];
+
+  expect(
+    await render_md(
+      <context.Provider
+        value={
+          {
+            domain: "🧨",
+            moderation: {
+              organization: { cached_libelle: "🦄" },
+              user: { email: "💌" },
+            },
+            query_suggest_same_user_emails,
+          } as Values
+        }
+      >
+        <Response />
+      </context.Provider>,
+    ),
+  ).toMatchInlineSnapshot(`
+    "Bonjour,
+
+    Nous avons bien reçu votre demande de rattachement à l&#39;organisation « 🦄 » sur ProConnect (anciennement : AgentConnect, MonComptePro).
+
+    Vous possédez déjà un compte ProConnect associé à l’adresse e-mail professionnelle : « ???? ».
+
+    Merci de bien vouloir vous connecter avec le compte déjà existant.
+
+    Votre adresse e-mail associée à un nom de domaine gratuit tel que « 🧨 » ne sera pas autorisée.
+
+    Bien cordialement,
+    L’équipe ProConnect.
+    "
+  `);
 });
 
 function Response() {
