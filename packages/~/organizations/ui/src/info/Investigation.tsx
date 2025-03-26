df0c1bff275f1321fb7bca5d9dadb7a17ab2c088
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-03-26 10:27:03 +0100

    refactor(moderation): Update visual of leaders list

--- packages/~/organizations/ui/src/info/Investigation.tsx
+++ packages/~/organizations/ui/src/info/Investigation.tsx
@@ -1,6 +1,7 @@
 //
 
 import { button } from "@~/app.ui/button";
+import { hx_urls } from "@~/app.urls";
 
 import type { Organization } from "@~/organizations.lib/entities/Organization";
 import type { JSX } from "hono/jsx";
@@ -13,14 +14,18 @@ type Props = JSX.IntrinsicElements["section"] & {
 
 //
 
-export function Investigation(props: Props) {
+export async function Investigation(props: Props) {
   const { organization } = props;
+  const hx_organizations_leaders_props =
+    await hx_urls.organizations.leaders.$get({
+      query: { siret: organization.siret },
+    });
 
   return (
     <div class="mt-5 w-full bg-[#F6F6F6] p-3">
       <a
         href={`https://lannuaire.service-public.fr/recherche?where=${organization.cached_code_postal}&whoWhat=Mairie`}
-        class={`${button({ size: "sm", type: "tertiary" })} mr-2 bg-white`}
+        class={`${button({ size: "sm", type: "tertiary" })} mr-2 bg-white `}
         rel="noopener noreferrer"
         target="_blank"
       >
@@ -28,7 +33,7 @@ export function Investigation(props: Props) {
       </a>
       <a
         href={`https://lannuaire.service-public.fr/recherche?where=${organization.cached_code_postal}`}
-        class={`${button({ size: "sm", type: "tertiary" })} mr-2 bg-white`}
+        class={`${button({ size: "sm", type: "tertiary" })} mr-2 bg-white `}
         rel="noopener noreferrer"
         target="_blank"
       >
@@ -36,11 +41,14 @@ export function Investigation(props: Props) {
       </a>
       <a
         href={`https://annuaire-entreprises.data.gouv.fr/dirigeants/${organization.siret.substring(0, 9)}`}
-        class={`${button({ size: "sm", type: "tertiary" })} bg-white`}
+        class={`${button({ size: "sm", type: "tertiary" })} mr-2 bg-white `}
         rel="noopener noreferrer"
         target="_blank"
       >
-        Voir liste dirigeants - Annuaire entreprise API
+        Liste dirigeants - Annuaire entreprise API
+      </a>
+      <a class="" {...hx_organizations_leaders_props} hx-trigger="load">
+        <i class="text-center">Recherche des dirigeants...</i>
       </a>
     </div>
   );
