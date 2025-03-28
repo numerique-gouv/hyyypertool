dd470a6438abea2a6237876a93f089858d5f3977
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-03-28 10:41:38 +0100

    chore(moderation): add elements acceptal modal

--- packages/~/moderations/ui/src/Actions/AcceptModal.tsx
+++ packages/~/moderations/ui/src/Actions/AcceptModal.tsx
@@ -1,9 +1,11 @@
-import { hx_include } from "@~/app.core/htmx";
+import { Htmx_Events, hx_include } from "@~/app.core/htmx";
 import { button } from "@~/app.ui/button";
 import { hx_urls } from "@~/app.urls";
 import { useContext } from "hono/jsx";
+import { AddAsMemberExternal } from "./AddAsMemberExternal";
+import { AddAsMemberInternal } from "./AddAsMemberInternal";
 import { AddDomain } from "./AddDomain";
-import { valid_context } from "./context";
+import { context, valid_context } from "./context";
 
 export async function AcceptModal({
   userEmail,
@@ -14,6 +16,7 @@ export async function AcceptModal({
 }) {
   const context_value = useContext(valid_context);
   const { $add_domain } = context_value;
+  const { $decision_form, $accept } = useContext(context);
   const hx_path_validate_moderation = await hx_urls.moderations[
     ":id"
   ].$procedures.validate.$patch({
@@ -47,9 +50,25 @@ export async function AcceptModal({
         {...hx_path_validate_moderation}
         hx-include={hx_include([$add_domain])}
         hx-swap="none"
+        _={`
+            on load if #${$accept}.checked then remove @hidden end
+            on change from #${$decision_form}
+              if #${$accept}.checked then remove @hidden else add @hidden end
+            on submit
+              wait for ${Htmx_Events.enum.afterOnLoad}
+              go to the top of body smoothly
+              wait 2s
+              go back
+          `}
       >
         <div class="mb-5">
           <AddDomain />
+        </div>
+        <div class="mb-5">
+          <AddAsMemberInternal />
+        </div>
+        <div class="mb-5">
+          <AddAsMemberExternal />
         </div>
         <div>
           <button
@@ -57,10 +76,10 @@ export async function AcceptModal({
               on click
                 add .hidden to #acceptModal
             `}
-            class={button()}
+            class={`${button()} w-full justify-center`}
             type="submit"
           >
-            Terminer
+            Notifier le membre et terminer
           </button>
         </div>
       </form>
