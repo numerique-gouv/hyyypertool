4320ceea7515b939a2e9d4ef2f459aa8a6557657
Douglas DUTEIL <douglasduteil@gmail.com>
2025-04-25 14:39:04 +0200

    style: remove extra partial

--- packages/~/moderations/api/src/:id/page.tsx
+++ packages/~/moderations/api/src/:id/page.tsx
@@ -2,7 +2,7 @@
 
 import { hx_trigger_from_body } from "@~/app.core/htmx";
 import { button } from "@~/app.ui/button";
-import { hx_urls } from "@~/app.urls";
+import { hx_urls, urls } from "@~/app.urls";
 import { MODERATION_EVENTS } from "@~/moderations.lib/event";
 import { IsUserExternalMember } from "@~/moderations.lib/usecase/IsUserExternalMember";
 import { Actions } from "@~/moderations.ui/Actions";
@@ -38,116 +38,114 @@ export default async function Moderation_Page() {
   } = usePageRequestContext();
 
   return (
-    <>
-      <main
-        class="fr-container my-12"
-        hx-disinherit="*"
-        {...await hx_urls.moderations[":id"].$get(
-          {
-            param: { id: moderation.id.toString() },
-          },
-          {},
-        )}
-        hx-select="main"
-        hx-trigger={hx_trigger_from_body([
-          MODERATION_EVENTS.Enum.MODERATION_UPDATED,
-        ])}
+    <main
+      class="fr-container my-12"
+      hx-disinherit="*"
+      {...await hx_urls.moderations[":id"].$get(
+        {
+          param: { id: moderation.id.toString() },
+        },
+        {},
+      )}
+      hx-select="main"
+      hx-trigger={hx_trigger_from_body([
+        MODERATION_EVENTS.Enum.MODERATION_UPDATED,
+      ])}
+    >
+      <button
+        _="on click go back"
+        class={button({
+          class: "fr-btn--icon-left fr-icon-arrow-go-back-fill",
+          type: "tertiary",
+          size: "sm",
+        })}
       >
-        <button
-          _="on click go back"
-          class={button({
-            class: "fr-btn--icon-left fr-icon-checkbox-circle-line",
-            type: "tertiary",
-            size: "sm",
-          })}
-        >
-          retour
-        </button>
-        <hr class="bg-none pt-6" />
+        retour
+      </button>
+      <hr class="bg-none pt-6" />
 
-        <Header.Provier value={{ moderation }}>
-          <Header />
-        </Header.Provier>
+      <Header.Provier value={{ moderation }}>
+        <Header />
+      </Header.Provier>
 
-        <hr class="my-3" />
+      <hr class="my-3" />
 
-        <div class="grid grid-cols-2 gap-6">
-          <About_User user={moderation.user} />
-          <div>
-            <h3>
-              <a
-                href={
-                  urls.organizations[":id"].$url({
-                    param: {
-                      id: moderation.organization.id.toString(),
-                    },
-                  }).pathname
-                }
-              >
-                🏛 Organisation
-              </a>
-            </h3>
-            <About_Organization organization={organization_fiche} />
-          </div>
+      <div class="grid grid-cols-2 gap-6">
+        <About_User user={moderation.user} />
+        <div>
+          <h3>
+            <a
+              href={
+                urls.organizations[":id"].$url({
+                  param: {
+                    id: moderation.organization.id.toString(),
+                  },
+                }).pathname
+              }
+            >
+              🏛 Organisation
+            </a>
+          </h3>
+          <About_Organization organization={organization_fiche} />
         </div>
+      </div>
 
-        <hr class="bg-none pt-6" />
+      <hr class="bg-none pt-6" />
 
-        <div class="grid grid-cols-2 gap-6">
-          <Investigation_User
-            user={moderation.user}
-            organization={moderation.organization}
-          />
-          <Investigation_Organization organization={moderation.organization} />
-        </div>
-
-        <hr class="bg-none pt-6" />
-
-        <OrganizationsByUser
+      <div class="grid grid-cols-2 gap-6">
+        <Investigation_User
           user={moderation.user}
-          query_organization_count={CountUserMemberships({
+          organization={moderation.organization}
+        />
+        <Investigation_Organization organization={moderation.organization} />
+      </div>
+
+      <hr class="bg-none pt-6" />
+
+      <OrganizationsByUser
+        user={moderation.user}
+        query_organization_count={CountUserMemberships({
+          pg: moncomptepro_pg,
+        })}
+      />
+      <hr class="my-3" />
+
+      <DomainsByOrganization
+        organization={moderation.organization}
+        query_domain_count={query_domain_count}
+      />
+
+      <hr class="my-3 bg-none" />
+
+      <UsersByOrganization
+        organization={moderation.organization}
+        query_members_count={query_organization_members_count}
+      />
+
+      <hr class="my-3 bg-none" />
+
+      <Actions
+        value={{
+          moderation,
+          query_suggest_same_user_emails: SuggestSameUserEmails({
             pg: moncomptepro_pg,
-          })}
-        />
-        <hr class="my-3" />
+          }),
+          query_is_user_external_member: IsUserExternalMember({
+            pg: moncomptepro_pg,
+          }),
+          query_suggest_organization_domains: SuggestOrganizationDomains({
+            pg: moncomptepro_pg,
+          }),
+        }}
+      />
 
-        <DomainsByOrganization
-          organization={moderation.organization}
-          query_domain_count={query_domain_count}
-        />
+      <hr class="my-3 bg-none" />
 
-        <hr class="my-3 bg-none" />
+      <hr />
 
-        <UsersByOrganization
-          organization={moderation.organization}
-          query_members_count={query_organization_members_count}
-        />
+      <hr class="my-3 bg-none" />
 
-        <hr class="my-3 bg-none" />
-
-        <Actions
-          value={{
-            moderation,
-            query_suggest_same_user_emails: SuggestSameUserEmails({
-              pg: moncomptepro_pg,
-            }),
-            query_is_user_external_member: IsUserExternalMember({
-              pg: moncomptepro_pg,
-            }),
-            query_suggest_organization_domains: SuggestOrganizationDomains({
-              pg: moncomptepro_pg,
-            }),
-          }}
-        />
-
-        <hr class="my-3 bg-none" />
-
-        <hr />
-
-        <hr class="my-3 bg-none" />
-
-        <Moderation_Exchanges />
-      </main>
-    </>
+      <Moderation_Exchanges />
+    </main>
   );
 }
