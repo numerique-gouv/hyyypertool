e5f1f709bc55ba1ce3aa0c518774cf5a5a049b9c
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-04-28 14:51:50 +0200

    refactor(toolbar): Delete the Actions section to have only the toolbar

--- packages/~/moderations/ui/src/Actions/Actions.tsx
+++ packages/~/moderations/ui/src/Actions/Actions.tsx
@@ -2,14 +2,8 @@
 
 import { hyper_ref } from "@~/app.core/html";
 import { z_email_domain } from "@~/app.core/schema/z_email_domain";
-import { button } from "@~/app.ui/button";
-import { hx_urls } from "@~/app.urls";
-import { MessageInfo } from "@~/moderations.ui/MessageInfo";
 import { AcceptModal } from "./AcceptModal";
 import { context, type Values } from "./context";
-import { Desicison } from "./Desicison";
-import { MemberInvalid } from "./MemberInvalid";
-import { MemberValid } from "./MemberValid";
 import { RefusalModal } from "./RefusalModal";
 import { Toolbar } from "./Toolbar";
 
@@ -23,12 +17,6 @@ export async function Actions({ value }: ActionProps) {
   const { moderation } = value;
 
   const { user } = moderation;
-
-  const hx_moderation_reprocess_props = await hx_urls.moderations[
-    ":id"
-  ].$procedures.reprocess.$patch({
-    param: { id: moderation.id.toString() },
-  });
 
   const domain = z_email_domain.parse(moderation.user.email, {
     path: ["user.email"],
@@ -44,28 +32,6 @@ export async function Actions({ value }: ActionProps) {
         ...value,
       }}
     >
-      <div class="bg-[var(--background-alt-blue-france)] p-8">
-        <h2>Actions de modération</h2>
-
-        <MessageInfo moderation={moderation} />
-
-        <hr class="bg-none" />
-        {moderation.moderated_at ? (
-          <button
-            class={button({ size: "sm", type: "tertiary" })}
-            {...hx_moderation_reprocess_props}
-            hx-swap="none"
-          >
-            Retraiter
-          </button>
-        ) : (
-          <>
-            <Desicison />
-            <MemberValid />
-            <MemberInvalid />
-          </>
-        )}
-      </div>
       <Toolbar />
       <AcceptModal userEmail={user.email} moderation={moderation} />
       <RefusalModal userEmail={user.email} />
