a765e4d146559b9194215da842fd4788c227469f
Douglas DUTEIL <douglasduteil@gmail.com>
2025-05-19 09:56:06 +0200

    fix: update snapshot

--- packages/~/moderations/ui/src/Actions/responses/enseignement_agricole.tsx
+++ packages/~/moderations/ui/src/Actions/responses/enseignement_agricole.tsx
@@ -19,7 +19,7 @@ export default function template() {
 
     Nous avons bien reçu votre demande de rattachement à l'organisation « ${organization_name} » sur ProConnect.
 
-    Votre adresse mail d’enseignement agricole : « ${domaine} » ne vous permet pas de rattacher votre compte utilisateur ProConnect au « ${organization_name} ».
+    Votre adresse mail d’enseignement agricole : « ${domain} » ne vous permet pas de rattacher votre compte utilisateur ProConnect au « ${organization_name} ».
     En tant qu'enseignant, merci de bien vouloir renouveler votre inscription en rattachant votre compte à l'Établissement Public Local d'Enseignement et de Formation Professionnelle Agricoles (EPLEFPA) dans lequel vous exercez.
 
     Bien cordialement,
