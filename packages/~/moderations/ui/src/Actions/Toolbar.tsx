a15e1330895da220789d445c9b43dad1d843695f
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-05-23 09:52:06 +0200

    refacto(toolbar): divides the component into smaller components

--- packages/~/moderations/ui/src/Actions/Toolbar.tsx
+++ packages/~/moderations/ui/src/Actions/Toolbar.tsx
@@ -1,16 +1,44 @@
-export function Toolbar({ moderation }: any) {
-  const showToolbar = moderation.moderated_at ? false : true;
+import type { Moderation } from "@~/moderations.lib/entities/Moderation";
+type ToolbarProps = {
+  moderation: Pick<Moderation, "moderated_at">;
+};
+export function Toolbar(props: ToolbarProps) {
   return (
     <div
       class="fixed bottom-0 right-0 z-50 flex w-full justify-end overflow-hidden bg-[--blue-france-975-75] p-2"
       role="dialog"
       aria-modal="true"
     >
-      {showToolbar && (
-        <div>
-          <button
-            class="fr-btn fr-mr-2w fr-btn--secondary bg-white"
-            _={`
+      <ModerationButtonGroup {...props} />
+      <SeeExchangesButton />
+    </div>
+  );
+}
+
+function SeeExchangesButton() {
+  return (
+    <a
+      href="#exchange_moderation"
+      class="fr-btn fr-btn--secondary bg-white"
+      _={`
+        on click
+          set #exchange_details.open to true
+          add .hidden to #refusalModal
+          add .hidden to #acceptModal
+          return true
+      `}
+    >
+      💬 Voir les échanges
+    </a>
+  );
+}
+function ModerationButtonGroup({ moderation }: ToolbarProps) {
+  if (moderation.moderated_at) return null;
+  return (
+    <>
+      <button
+        class="fr-btn fr-mr-2w fr-btn--secondary bg-white"
+        _={`
                   on click
                     if #refusalModal.classList.contains('hidden') is false
                         add .hidden to #refusalModal
@@ -22,13 +50,13 @@ export function Toolbar({ moderation }: any) {
                       add .hidden to #acceptModal
                     end
                 `}
-          >
-            ✅ Accepter
-          </button>
+      >
+        ✅ Accepter
+      </button>
 
-          <button
-            class="fr-btn fr-mr-2w fr-btn--secondary bg-white"
-            _={`
+      <button
+        class="fr-btn fr-mr-2w fr-btn--secondary bg-white"
+        _={`
                   on click
                     if #acceptModal.classList.contains('hidden') is false
                       add .hidden to #acceptModal
@@ -40,24 +68,9 @@ export function Toolbar({ moderation }: any) {
                       add .hidden to #refusalModal
                     end
                 `}
-          >
-            ❌ Refuser
-          </button>
-        </div>
-      )}
-      <a
-        href="#exchange_moderation"
-        class="fr-btn fr-btn--secondary bg-white"
-        _={`
-              on click
-                set #exchange_details.open to true
-                add .hidden to #refusalModal
-                add .hidden to #acceptModal
-                return true
-            `}
       >
-        💬 Voir les échanges
-      </a>
-    </div>
+        ❌ Refuser
+      </button>
+    </>
   );
 }
