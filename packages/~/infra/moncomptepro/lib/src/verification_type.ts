09c0b1ddd40b1b8f682a73608eeb11ca14edf0af
Douglas DUTEIL <douglasduteil@gmail.com>
2025-05-28 17:04:53 +0200

    fix(organization): remove legacy verification_type

--- packages/~/infra/moncomptepro/lib/src/verification_type.ts
+++ packages/~/infra/moncomptepro/lib/src/verification_type.ts
@@ -1,9 +1,10 @@
 //
 
-import  { UserOrganizationLinkVerificationTypeSchema } from "@gouvfr-lasuite/proconnect.identite/types";
 import type { UserOrganizationLinkVerificationType } from "@gouvfr-lasuite/proconnect.identite/types";
+import { UserOrganizationLinkVerificationTypeSchema } from "@gouvfr-lasuite/proconnect.identite/types";
 
 //
 
-export const Verification_Type_Schema: typeof UserOrganizationLinkVerificationTypeSchema = UserOrganizationLinkVerificationTypeSchema
+export const Verification_Type_Schema =
+  UserOrganizationLinkVerificationTypeSchema;
 export type Verification_Type = UserOrganizationLinkVerificationType;
