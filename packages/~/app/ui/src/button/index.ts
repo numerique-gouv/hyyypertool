6784e65f50c07655642c7fe90310b17e7911d68a
Douglas DUTEIL <douglasduteil@gmail.com>
2025-05-28 15:18:42 +0200

    chore: put dsfr on a layer

--- packages/~/app/ui/src/button/index.ts
+++ packages/~/app/ui/src/button/index.ts
@@ -10,16 +10,16 @@ export const button = tv({
     intent: {
       danger: "bg-(--error-425-625) hover:bg-(--error-425-625-hover)!",
       dark: "bg-(--grey-0-1000) hover:bg-(--grey-200-850)!",
+      ghost:
+        "bg-transparent text-black hover:bg-(--background-default-grey-hover)!",
       success:
         "bg-(--background-action-high-green-bourgeon) hover:enabled:bg-(--background-action-high-green-bourgeon-hover)!",
       warning:
         "bg-(--background-action-high-warning) hover:bg-(--background-action-high-warning-hover)!",
-      ghost:
-        "bg-transparent text-black hover:bg-(--background-default-grey-hover)!",
     },
     size: {
-      sm: "fr-btn--sm",
       lg: "fr-btn--lg",
+      sm: "fr-btn--sm",
     },
     type: {
       tertiary: "fr-btn--tertiary",
