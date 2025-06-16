c6cf14b622f7b59d032a60985e0944fe2ae5ccd9
Douglas DUTEIL <douglasduteil@gmail.com>
2025-06-16 09:43:39 +0200

    debug: issue on https raw req
    req.url seems to return an http url instead of a https url

--- packages/~/auth/api/src/index.ts
+++ packages/~/auth/api/src/index.ts
@@ -93,17 +93,19 @@ export default new Hono<Oidc_Context & App_Context>()
       const session = c.get("session");
       const { redirect, req, get } = c;
 
-      req.valid("query");
-
       const config = get("oidc_config");
 
-      console.log(req.url);
-      console.log(get_redirect_uri(req.url));
+      const redirect_uri = get_redirect_uri(req.url);
       console.trace();
-      const tokens = await authorizationCodeGrant(config, req.raw, {
-        expectedNonce: session.get("nonce"),
-        expectedState: session.get("state"),
-      });
+      const tokens = await authorizationCodeGrant(
+        config,
+        new URL(redirect_uri),
+        {
+          expectedNonce: session.get("nonce"),
+          expectedState: session.get("state"),
+        },
+        req.query(),
+      );
 
       const claims = tokens.claims();
       const userinfo = await fetchUserInfo(
