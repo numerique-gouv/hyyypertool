b7e8bf1adf0e0641120cdd49a442639b73978f34
Douglas Duteil <douglasduteil@gmail.com>
2025-07-29 13:09:02 +0200

    refactor(html): add optional human-readable names to hyper_ref IDs (#1045)

--- sources/app/core/src/html/index.ts
+++ sources/app/core/src/html/index.ts
@@ -4,6 +4,9 @@ import { createHash } from "node:crypto";
 
 //
 
-export function hyper_ref() {
-  return `hyyyper_${createHash("sha1").update(crypto.randomUUID()).digest("hex")}`;
+export function hyper_ref(given_name?: string) {
+  const short_sha = createHash("sha1").update(crypto.randomUUID()).digest("hex").slice(0, 8);
+  return given_name 
+    ? `hyyyper_${given_name}_${short_sha}`
+    : `hyyyper_${short_sha}`;
 }
